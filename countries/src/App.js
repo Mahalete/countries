011609d0fb928c965b5da53e28b7d64bf0f324a9
BC-macair011 <mahialemu@gmail.com>
2021-11-23 11:42:02 +0200

    render card of countries with their capital,flags and currency, population

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,11 +1,55 @@
-import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
+import number from "easy-number-formatter";
 
-function App() {
-  return (
-    <div>
-      <h1>Countries</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://restcountries.com/v2/all?fields=name,capital,flags,languages,currencies,population"
+      )
+      .then((res) => {
+        this.setState({ data: res.data });
+        console.log(this.state.data);
+      });
+  }
+
+  render() {
+    return (
+      <div className="countries">
+        {this.state.data.map((c) => (
+          <div className="country" key={c.name}>
+            <h2> {c.name}</h2> <h3>{c.capital}</h3>
+            <img src={c.flags.png} alt={c.name} />
+            <div className="cardContent">
+              <p>
+                Language(s):
+                {c.languages.map((lang, i) => (
+                  <span key={i}> {lang.name} </span>
+                ))}
+              </p>
+              <p>
+                Currencies:
+                {c.currencies.map((mon, i) => (
+                  <span key={i}>
+                    {mon.name} - {mon.symbol}
+                  </span>
+                ))}
+              </p>
+              <p>
+                Population:
+                <span className="low">{number.formatNumber(c.population)}</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;

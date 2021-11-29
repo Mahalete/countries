afe22c5b219bec91c3b238f483894e8801b684be
BC-macair011 <mahialemu@gmail.com>
2021-11-29 14:04:14 +0200

    Clean up code and Add Navigation with new Routes

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,89 +1,40 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Spinner from "react-spinner-material";
-import number from "easy-number-formatter";
+import React from "react";
+import Home from "./components/Home";
+import CountriesList from "./components/CountriesList";
+import {
+  BrowserRouter,
+  Link,
+  Routes,
+  Route,
+  useParams,
+} from "react-router-dom";
+import CountrySingle from "./components/CountrySingle";
 
-class App extends Component {
-  state = {
-    data: [],
-    searchInput: "",
-    isLoading: true,
-  };
+const RouteWrapper = (props) => {
+  const params = useParams();
+  return <CountrySingle params={params} {...props} />;
+};
+const App = () => {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/countries">Countries</Link>
+          </li>
+        </ul>
+      </nav>
 
-  componentDidMount() {
-    axios
-      .get(
-        "https://restcountries.com/v2/all?fields=name,capital,flags,languages,currencies,population"
-      )
-      .then((res) => {
-        this.setState({ data: res.data, isLoading: false });
-        console.log(this.state.data);
-      });
-  }
-  searchHandler = (e) => {
-    //if you want to use ES5 functions with class you need to use bind
-    this.setState({ searchInput: e.target.value });
-    console.log(this.state.searchInput);
-  };
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/countries" element={<CountriesList />} />
+        <Route path="/countries/:name" element={<RouteWrapper />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-  render() {
-    //same as if (this.state.isLoading === true)
-    if (this.state.isLoading) {
-      return (
-        <div>
-          <p>
-            Wait, I am loading{" "}
-            <Spinner radius={100} color={"#333"} stroke={6} visible={true} />
-          </p>
-        </div>
-      );
-    }
-    if (!this.state.isloading) {
-      return (
-        <div>
-          <header>
-            <h1>Countries List</h1>
-          </header>
-          <input type="text" name="search" onChange={this.searchHandler} />
-          <div className="countries">
-            {this.state.data
-              .filter((c) => {
-                return c.name
-                  .toLowerCase()
-                  .includes(this.state.searchInput.toLowerCase());
-              })
-              .map((c) => (
-                <div className="country" key={c.name}>
-                  <h2> {c.name}</h2> <h3>{c.capital}</h3>
-                  <img src={c.flags.png} alt={c.name} />
-                  <div className="cardContent">
-                    <p>
-                      Language(s):
-                      {c.languages.map((lang, i) => (
-                        <span key={i}> {lang.name} </span>
-                      ))}
-                    </p>
-                    <p>
-                      Currencies:
-                      {c.currencies.map((mon, i) => (
-                        <span key={i}>
-                          {mon.name} - {mon.symbol}
-                        </span>
-                      ))}
-                    </p>
-                    <p>
-                      Population:
-                      <span className="low">
-                        {number.formatNumber(c.population)}
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
-      );
-    }
-  }
-}
 export default App;
---
+++ countries/src/components/CountriesList.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Spinner from "react-spinner-material";
+import CountryCard from "./CountryCard";
+import { LVAL_TYPES } from "@babel/types";
+
+class CountriesList extends Component {
+  state = {
+    data: [],
+    searchInput: "",
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://restcountries.com/v2/all?fields=name,capital,flags,languages,currencies,population,region"
+      )
+      .then((res) => {
+        this.setState({ data: res.data, isLoading: false });
+        console.log(this.state.data);
+      });
+  }
+  searchHandler = (e) => {
+    //if you want to use ES5 functions with class you need to use bind
+    this.setState({ searchInput: e.target.value });
+    console.log(this.state.searchInput);
+  };
+
+  render() {
+    //same as if (this.state.isLoading === true)
+    if (this.state.isLoading) {
+      return (
+        <div>
+          <p>
+            Wait, I am loading{" "}
+            <Spinner radius={100} color={"#333"} stroke={6} visible={true} />
+          </p>
+        </div>
+      );
+    }
+    if (!this.state.isloading) {
+      return (
+        <div>
+          <header>
+            <h1>Countries List</h1>
+          </header>
+          <input type="text" name="search" onChange={this.searchHandler} />
+          <div className="countries">
+            {this.state.data
+              .filter((c) => {
+                return c.name
+                  .toLowerCase()
+                  .includes(this.state.searchInput.toLowerCase());
+              })
+              .map((c) => (
+                <CountryCard {...c} key={c.name} />
+              ))}
+          </div>
+        </div>
+      );
+    }
+  }
+}
+export default CountriesList;

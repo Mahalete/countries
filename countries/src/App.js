01c610ca8990631c7cd8e847ffd926b032d8829a
BC-macair011 <mahialemu@gmail.com>
2021-12-13 18:25:56 +0200

    added Readme file

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 // import "./App.css";
+
 import Home from "./components/Home";
 import CountriesList from "./components/CountriesList";
 import {
@@ -18,34 +19,36 @@ const RouteWrapper = (props) => {
 };
 const App = () => {
   return (
-    <BrowserRouter>
-      <nav className="bg-yellow-500">
-        <ul className="flex border-b">
-          <li className="-mb-px mr-1 mt-4 ml-2">
-            <Link
-              className=" inline-block  py-2 px-4 text-white font-semibold text-2xl"
-              to="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li className="-mb-px mr-1 mt-4 ">
-            <Link
-              className="inline-block  py-2 px-4 text-white font-semibold text-2xl "
-              to="/countries"
-            >
-              Countries
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <BrowserRouter>
+        <nav className="bg-yellow-500">
+          <ul className="flex border-b">
+            <li className="-mb-px mr-1 mt-4 ml-2">
+              <Link
+                className=" inline-block  py-2 px-4 text-white font-semibold text-2xl"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="-mb-px mr-1 mt-4 ">
+              <Link
+                className="inline-block  py-2 px-4 text-white font-semibold text-2xl "
+                to="/countries"
+              >
+                Countries
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/countries" element={<CountriesList />} />
-        <Route path="/countries/:name" element={<RouteWrapper />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/countries" element={<CountriesList />} />
+          <Route path="/countries/:name" element={<RouteWrapper />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
